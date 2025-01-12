bb6d0d92b04a8fc681e9238fcc01f19f96c8a0fb
lunatix-lab <escravosdopoder@yahoo.com.br>
2025-01-12 17:27:37 -0300

    [LANDING] Add landing page
    Add landing page with links to the blog that comes soon.
    
    signed-off-by: Lunatix <escravosdopoder@yahoo.com.br>

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,13 +1,13 @@
-function menuShow() {
+function menuShow(path='./') {
     let menuMobile = document.querySelector('.mobile-menu');
     if(menuMobile.classList.contains('open'))
     {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "src/images/menu_white_36dp.svg";
+        document.querySelector('.icon').src = path + "src/images/menu_white_36dp.svg";
     }
     else
     {
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "src/images/close_white_36dp.svg";
+        document.querySelector('.icon').src = path + "src/images/close_white_36dp.svg";
     }
 }
